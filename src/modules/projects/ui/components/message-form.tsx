72f8c0db1a107a39260c1b6bf2c932405d27b13b
akbar-farajov <akbarfarajov74@gmail.com>
2025-10-03 19:01:56 +0400

    Refactor MessageForm component structure for improved readability
    - Removed unnecessary wrapping `Form` component around the `form` element.
    - Streamlined the layout of the message input and button for better clarity and maintainability.

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -58,57 +58,55 @@ export const MessageForm: React.FC<Props> = ({ projectId }) => {
   const isDisabled = isPending || !form.formState.isValid;
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="relative  border rounded-lg p-4 pt-1 bg-sidebar "
-      >
-        <FormField
-          control={form.control}
-          name="value"
-          render={({ field }) => (
-            <TextareaAutosize
-              disabled={isPending}
-              {...field}
-              onFocus={() => {
-                setIsFocused(true);
-              }}
-              onBlur={() => {
-                setIsFocused(false);
-              }}
-              minRows={2}
-              maxRows={4}
-              className="w-full  resize-none border-none pt-4 outline-none bg-transparent"
-              placeholder="Type your message..."
-              onKeyDown={(e) => {
-                if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
-                  e.preventDefault();
-                  form.handleSubmit(onSubmit)(e);
-                }
-              }}
-            />
-          )}
-        />
-        <div className="flex gap-2 items-end justify-between pt-2">
-          <div className="text-sm text-muted-foreground font-mono">
-            <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center justify-center rounded border bg-muted px-1 font-mono text-[10px] font-medium text-muted-foreground ">
-              <span>&#8984;</span>Enter
-            </kbd>
-            &nbsp;to send
-          </div>
-          <Button
-            className={cn("size-8 rounded-full")}
-            disabled={isDisabled}
-            onClick={() => form.handleSubmit(onSubmit)(undefined)}
-          >
-            {isPending ? (
-              <Loader2 className="w-4 h-4" />
-            ) : (
-              <SendIcon className="w-4 h-4" />
-            )}
-          </Button>
+    <form
+      onSubmit={form.handleSubmit(onSubmit)}
+      className="relative  border rounded-lg p-4 pt-1 bg-sidebar "
+    >
+      <FormField
+        control={form.control}
+        name="value"
+        render={({ field }) => (
+          <TextareaAutosize
+            disabled={isPending}
+            {...field}
+            onFocus={() => {
+              setIsFocused(true);
+            }}
+            onBlur={() => {
+              setIsFocused(false);
+            }}
+            minRows={2}
+            maxRows={4}
+            className="w-full  resize-none border-none pt-4 outline-none bg-transparent"
+            placeholder="Type your message..."
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+                e.preventDefault();
+                form.handleSubmit(onSubmit)(e);
+              }
+            }}
+          />
+        )}
+      />
+      <div className="flex gap-2 items-end justify-between pt-2">
+        <div className="text-sm text-muted-foreground font-mono">
+          <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center justify-center rounded border bg-muted px-1 font-mono text-[10px] font-medium text-muted-foreground ">
+            <span>&#8984;</span>Enter
+          </kbd>
+          &nbsp;to send
         </div>
-      </form>
-    </Form>
+        <Button
+          className={cn("size-8 rounded-full")}
+          disabled={isDisabled}
+          onClick={() => form.handleSubmit(onSubmit)(undefined)}
+        >
+          {isPending ? (
+            <Loader2 className="w-4 h-4" />
+          ) : (
+            <SendIcon className="w-4 h-4" />
+          )}
+        </Button>
+      </div>
+    </form>
   );
 };
